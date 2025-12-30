3263877ddee1d3119593366ffeec3eb5c5a65c41
Oliver Ah You <ahyouoliver9@gmail.com>
2025-12-30 09:42:14 +0400

    Week1Day4Daily Challenge completed

---
+++ Week1/Week2/Week2Day2Exercises.py
@@ -0,0 +1,18 @@
+#Exercise 1: What Are You Learning?
+def display_message():
+    message = "I am learning about functions in Python."
+    print(message)
+
+display_message()
+
+#Exercise 2: What’s Your Favorite Book?
+def favorite_book(title):
+    print(f"One of my favorite books is '{title}'.")
+favorite_book("Can't Hurt Me by David Goggins")
+
+#Exercise 3: Some Geography
+
+#Exercise 4: Random
+#Exercise 5: Let’s Create Some Personalized Shirts!
+#Exercise 6: Magicians…
+#Exercise 7: Temperature Advice
